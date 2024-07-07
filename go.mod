4c8437c504d8ff718eebcae3c719d8e05ae85b2c
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-07-07 08:07:11 +0000

    Update

--- go.mod
+++ go.mod
@@ -1,3 +1,24 @@
 module myproject
 
 go 1.22.4
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/bytedance/sonic v1.11.8 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/fasthttp/router v1.5.1 // indirect
+	github.com/grbit/go-json v0.11.0 // indirect
+	github.com/klauspost/compress v1.17.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
+	github.com/mymmrac/telego v0.30.2 // indirect
+	github.com/sashabaranov/go-openai v1.26.2 // indirect
+	github.com/savsgio/gotils v0.0.0-20240303185622-093b76447511 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.54.0 // indirect
+	github.com/valyala/fastjson v1.6.4 // indirect
+	golang.org/x/arch v0.6.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,81 @@
-package ChatGPT_bot_telegram_go
+package main
+
+import (
+	"context"
+	"fmt"
+	openai "github.com/sashabaranov/go-openai"
+	"os"
+
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
+
+func main() {
+
+	botToken := ""
+
+	bot, err := telego.NewBot(botToken, telego.WithDefaultDebugLogger())
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	updates, _ := bot.UpdatesViaLongPolling(nil)
+
+	bh, _ := th.NewBotHandler(bot, updates)
+
+	defer bh.Stop()
+	defer bot.StopLongPolling()
+
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		chatId := tu.ID(update.Message.Chat.ID)
+
+		keyboard := tu.Keyboard(
+			tu.KeyboardRow(),
+		)
+		message := tu.Message(
+			chatId,
+			"Hello!",
+		).WithReplyMarkup(keyboard)
+
+		bot.SendMessage(message)
+
+	}, th.CommandEqual("start"))
+
+	bh.Handle(func(bot *telego.Bot, update telego.Update) {
+		chatID := tu.ID(update.Message.Chat.ID)
+		content := update.Message.Text
+		reqToGPT(content, bot, chatID)
+	}, th.AnyMessageWithText())
+
+	bh.Start()
+}
+
+func reqToGPT(content string, bot *telego.Bot, chatId telego.ChatID) {
+	client := openai.NewClient("token")
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model:       openai.GPT3Dot5Turbo,
+			MaxTokens:   150,
+			Temperature: 0,
+			N:           1,
+			Stop:        nil,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: content,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+	bot.SendMessage(tu.Message(chatId, resp.Choices[0].Message.Content))
+
+}
